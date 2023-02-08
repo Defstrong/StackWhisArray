143833d2fddb52e0394e1e23e5ecd7f025c931d4
Defstrong <sfayzulloev1@gmail.com>
2023-02-08 14:53:53 +0500

    Add project files.

---
+++ StackWhisArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackWhisArray.StackWhisArray;
+
+var stack = new CustomStack<int>();
+
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+stack.Pop();
+
+foreach(var ii in stack)
+    Console.WriteLine(ii);
---
+++ StackWhisArray/StackWhisArray/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+
+namespace StackWhisArray.StackWhisArray
+{
+    sealed class CustomStack<T> : IEnumerable<T>
+    {
+        private T[] stackArray;
+        private int MaxCount { get; set; }
+        public int Count { get; private set; }
+        public bool IsEmpty { get => Count == 0; }
+
+        public CustomStack(int maxCount = 100)
+        {
+            MaxCount = maxCount;
+            stackArray = new T[MaxCount];
+        }
+
+        public void Push(T item)
+        {
+            if(IsEmpty)
+            {
+                stackArray[MaxCount-1] = item;
+                Count++;
+                return;
+            }
+            stackArray[MaxCount - Count-1] = item;
+            Count++;
+        }
+        public T Pop()
+        {
+            if (IsEmpty)
+                return default;
+            var returnItem = stackArray[MaxCount - Count - 1];
+            stackArray[MaxCount - Count - 1] = default(T);
+            Count--;
+            return returnItem;
+        }
+        public T Peek
+        {
+            get
+            {
+                if (IsEmpty)
+                    return default;
+                return stackArray[MaxCount - Count - 1];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = MaxCount-Count; i < MaxCount; i++)
+            {
+                yield return stackArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
